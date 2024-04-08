7c15e41ccb5c8e7cac52694abe49078b224b0bd0
Charlotte Keitumetse Lekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-04-08 09:20:16 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -33,6 +33,14 @@ certificateContent.innerHTML = `
 
 `;
 
+const logo = document.createElement("img");
+logo.src = "./logo.png";
+logo.width = 200;
+
+certificateContent.appendChild(logo);
+const text = document.createElement("p");
+text.textContent = personalMessage;
+certificateContent.appendChild(text);
 
 
     // Display the modal
